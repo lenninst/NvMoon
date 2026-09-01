c73a4bc88ad7926636ade7b5203a5903614f3b90
lenninst <leninguaminga@gmail.com>
2026-09-01 03:03:01 -0500

    xd

--- lua/lenn/plugins/which-key.lua
+++ lua/lenn/plugins/which-key.lua
@@ -22,7 +22,7 @@ return {
         { "<leader>f", group = "Find" },
         { "<leader>h", group = "Git Hunk" },
         { "<leader>l", group = "LSP" },
-        { "<leader>u", group = " Utils" }
+        { "<leader>u", group = " Utils" }
       },
     },
     keys = {
